bb1a62524ca5391ca21807261bdad2a55b113a32
Baz <boruch11@gmail.com>
2019-08-15 19:30:01 +1000

    add google beta provider for cloud run
    Signed-off-by: Baz <boruch11@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -5,8 +5,20 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+provider "google-beta" {
+  credentials = "${var.credentials}"
+  project     = "${var.project_id}"
+  region      = "${var.region}"
+}
+
 output "gcp_credentials" {
   value       = google.credentials
+  description = "The credentials for authenticating with gcp."
+  sensitive   = true
+}
+
+output "gcp_beta_credentials" {
+  value       = google-beta.credentials
   description = "The credentials for authenticating with gcp."
   sensitive   = true
 }
@@ -14,7 +26,7 @@ output "gcp_credentials" {
 resource "google_cloud_run_service" "selfhydro-state" {
   name     = "selfhydro-state"
   location = "${var.region}"
-
+  provider = "google-beta"
   metadata {
     namespace = "${var.project_id}"
   }
