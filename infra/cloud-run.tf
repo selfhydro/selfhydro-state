5781d8a43561c337fdfd2eed5f6f9a86d8e89210
Baz <boruch11@gmail.com>
2019-08-15 19:21:52 +1000

    Add execute to unit test script

--- infra/cloud-run.tf
+++ infra/cloud-run.tf
@@ -1,15 +1,14 @@
-resource "google_cloud_run_service" "selfhydro-state" {
-  name     = "selfhydro-state"
-  location = "${var.region}"
-  provider = "google-beta"
-
-  metadata {
-    namespace = "${var.project_id}"
-  }
-
-  spec {
-    containers {
-	     image = "${var.cloud_run_image}"
-	   }
-  }
-}
+# resource "google_cloud_run_service" "selfhydro-state" {
+#   name     = "selfhydro-state"
+#   location = "${var.region}"
+#
+#   metadata {
+#     namespace = "${var.project_id}"
+#   }
+#
+#   spec {
+#     containers {
+# 	     image = "${var.cloud_run_image}"
+# 	   }
+#   }
+# }
--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,18 @@ output "gcp_credentials" {
   description = "The credentials for authenticating with gcp."
   sensitive   = true
 }
+
+resource "google_cloud_run_service" "selfhydro-state" {
+  name     = "selfhydro-state"
+  location = "${var.region}"
+
+  metadata {
+    namespace = "${var.project_id}"
+  }
+
+  spec {
+    containers {
+	     image = "${var.cloud_run_image}"
+	   }
+  }
+}
